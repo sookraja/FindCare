21f0642e28cfea694b3a88f4b8a1f8b7221925db
BLNDM-99 <blendmustafa71@gmail.com>
2025-03-11 02:00:41 +0000

    Puhsing app delegate to include firebase

--- FindCare/AppDelegate.swift
+++ FindCare/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,10 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
+    // MARK: UISceneSession Lifecycl
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
