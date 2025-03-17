77c49baa392e356b3df06259a2ae3bf9943b39ba
BLNDM-99 <blendmustafa71@gmail.com>
2025-03-17 05:12:54 +0000

    Added structs for when retrieving info from Firebase

--- FindCare/ViewController.swift
+++ FindCare/ViewController.swift
@@ -6,9 +6,34 @@
 //
 
 import UIKit
+import Foundation
+import FirebaseFirestore
+
+struct DBStaff : Codable {
+    let staffId: Int
+    let firstName: String
+    let lastName: String
+    let phoneNUmber: String
+    let role: String
+    let email: String
+    
+}
+
+struct DBDepartment : Codable {
+    let departmentId: String
+    let name: String
+    let description: String?
+    let staff: [DBStaff]
+}
 
 class ViewController: UIViewController {
 
+    private let departmentCollection = Firestore.firestore().collection("Departments")
+    
+    private func departmentDocument(departmentId: String) -> DocumentReference{
+        departmentCollection.document(departmentId)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
