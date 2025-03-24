a573fbda62fe78f7a7eb1bd15e422ab77cd0e397
Edgar Ponce Cartagena <ponceed@sheridancollege.ca>
2025-03-24 15:43:04 -0400

    Floor Plan Integration

---
+++ FindCare/FloorPlanOverlay.swift
@@ -0,0 +1,12 @@
+//
+//  FloorPlanOverlay.swift
+//  FindCare
+//
+//  Created by Edgar Ponce on 2025-03-24.
+//
+
+import UIKit
+
+class FloorPlanOverlay: NSObject {
+
+}
---
+++ FindCare/LocationManager.swift
@@ -0,0 +1,12 @@
+//
+//  LocationManager.swift
+//  FindCare
+//
+//  Created by Edgar Ponce on 2025-03-24.
+//
+
+import UIKit
+
+class LocationManager: CLLocation {
+
+}
