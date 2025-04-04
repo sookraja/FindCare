30e9e3254f29c9b66e520722e1fcbb813f0be99f
sookraja <sookraja@sheridancollege.ca>
2025-04-04 02:00:40 -0400

    ui updates (navigation button needs to be lowered)

---
+++ FindCare/HelpScreenViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HelpScreenViewController.swift
+//  FindCare
+//
+//  Created by Annette Sookraj on 2025-04-04.
+//
+
+import UIKit
+
+class HelpScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
